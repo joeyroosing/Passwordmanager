c0361aa6f13684abe12685dd690ec67e72389a67
Joey Roosing <j.roosing@outlook.com>
2015-03-12 12:26:58 +0100

    Introduced first draft of connection properties Created first version of connection manager

---
+++ passwordmanager-datamanager/src/main/java/com/svarasoft/passwordmanager/datamanager/H2ConnectionManager.java
@@ -0,0 +1,98 @@
+package com.svarasoft.passwordmanager.datamanager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Joey
+ */
+public class H2ConnectionManager implements ConnectionManager {
+
+    private static final Logger LOG = Logger.getLogger(H2ConnectionManager.class.getName());
+    
+    private static final String USER_HOME = System.getProperty("user.home");
+    private static final String PROPERTIES_LOCATION = USER_HOME + 
+            File.separator + "svarasoft" + File.separator + 
+            "passwordmanager" + File.separator + "database.properties";
+
+    private static H2ConnectionManager connectionManager;
+
+    private H2ConnectionManager() throws SQLException {
+        try {
+            // Load the driver
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static ConnectionManager getInstance() throws SQLException {
+        if (connectionManager == null) {
+            connectionManager = new H2ConnectionManager();
+        }
+        return connectionManager;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        Properties properties = loadProperties();
+
+        String url = "jdbc:h2:~/svarasoft/passwordmanager/pwmdb;CIPHER=AES";
+        String username = "sa";
+        String password = "h2defpass sapass4h2db";
+        if (properties != null) {
+            url = properties.getProperty("datasource.URL");
+            username = properties.getProperty("dataSource.user");
+            password = properties.getProperty("dataSource.filePass");
+            password += " " + properties.getProperty("dataSource.password");
+
+        }
+        Connection conn = DriverManager.getConnection(url, username, password);
+        return conn;
+    }
+
+    @Override
+    public void cleanupResources() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Loads properties from a file
+     *
+     * @return the loaded properties or null
+     */
+    // TODO add encryption to properties file
+    private Properties loadProperties() {
+        FileInputStream fis = null;
+        try {
+            Properties properties = new Properties(); 
+
+            // if file eixsts, load it, otherwise create with defaults
+            fis = new FileInputStream(PROPERTIES_LOCATION);
+            properties.load(fis);
+            return properties;
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, null, e);
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException ex) {
+                    LOG.log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return null;
+    }
+}
--- passwordmanager-gui/src/main/java/com/svarasoft/passwordmanager/init/GuiceModule.java
+++ passwordmanager-gui/src/main/java/com/svarasoft/passwordmanager/init/GuiceModule.java
@@ -12,33 +12,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Joey on 3/4/2015.
  */
 public class GuiceModule extends AbstractModule {
-
+    static final Logger LOG = Logger.getLogger(GuiceModule.class.getName());
+    
     @Override
     protected void configure() {
-        try {
-            bind(MainPresenter.class).toInstance(loadPresenter(Constants.MAIN_VIEW_FXML));
-            bind(PasswordDetailPresenter.class).toInstance(loadPresenter(Constants.PASSWORD_DETAIL_FXML));
-            bind(SafePresenter.class).toInstance(loadPresenter(Constants.SAFE_VIEW_FXML));
-        } catch (IOException e) {
-            // TODO add better handling
-            // NOTE: this is not best-practice for exception handling. Check the
-            // Guice documentation for better ways.
-            throw new RuntimeException(String.format("Error loading FXML file"), e);
-        }
+        bind(MainPresenter.class).toInstance(loadPresenter(Constants.MAIN_VIEW_FXML));
+        bind(PasswordDetailPresenter.class).toInstance(loadPresenter(Constants.PASSWORD_DETAIL_FXML));
+        bind(SafePresenter.class).toInstance(loadPresenter(Constants.SAFE_VIEW_FXML));
     }
 
     /**
      * Loads an FXML file into memory
      *
+     * @param <T> 
      * @param url the path to the fxml file
      * @return the presenter for that fxml file
      */
-    protected <T> T loadPresenter(String url) throws IOException {
+    protected <T> T loadPresenter(String url) {
         InputStream fxmlStream = null;
         try {
             fxmlStream = getClass().getResourceAsStream(url);
@@ -52,6 +49,9 @@ public class GuiceModule extends AbstractModule {
             loader.setResources(resourceBundle);
             loader.load(fxmlStream);
             return loader.getController();
+        } catch (IOException ex) {
+            Logger.getLogger(GuiceModule.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(String.format(Constants.LOADING_FXML_FAILED, url), ex);
         } finally {
             if (fxmlStream != null)  {
                 try  {
